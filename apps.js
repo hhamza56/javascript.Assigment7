ac7953da33ec519e4f94728869440dc6b8be6813
Muhammad Hamza <143834218+hhamza56@users.noreply.github.com>
2023-10-08 21:36:17 +0100

    work complete

---
+++ apps.js
@@ -0,0 +1,112 @@
+
+
+
+// question 01//
+
+// function get(){
+
+// }
+
+
+// question 02//
+
+// function sum(){
+
+// }
+
+
+
+// question 03//
+
+// function gat(){
+// }
+
+
+// question 04//
+
+// function greet(a){
+// console.log(a)
+//   a.style.background = "green";
+//   a.style.color = "red";
+// }
+
+// question 05//
+
+// function hat(a){
+//   console.log(a)
+// a.audio
+
+
+// }
+
+// question 7//
+
+// function get(){
+
+// }
+
+
+
+// question 08//
+
+// function get(b){
+//    b.style.Hover="red"
+//      b.style.background="red"
+//      b.style.color="white"
+//    console.log(b)
+// }
+
+
+
+
+// question 09//
+
+// function get(){
+
+// }
+
+
+// question 10//
+
+
+// function get(){
+
+// }
+
+
+
+// question 11//
+
+// function get(){
+// }
+
+
+// question 12//
+
+
+// function get(){
+
+
+//     var 
+
+//     console.log(resulr)
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
